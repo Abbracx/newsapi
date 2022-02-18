3c93e0b17ad963f1747f595237a53a98d600ffd3
Abbracx <tankoraphael@gmail.com>
2022-02-19 00:41:50 +0100

    news api basic CRUD with @api_view decorator for functional views.

---
+++ news/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from news.models import Article
+
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    author = serializers.CharField(max_length=50)
+    title = serializers.CharField(max_length=10)
+    description = serializers.CharField(max_length=200)
+    body = serializers.CharField()
+    location = serializers.CharField(max_length=120)
+    publication_date = serializers.DateField()
+    active = serializers.BooleanField(default=True)
+    created_at = serializers.DateField()
+    updated_at = serializers.DateField()
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.author = validated_data.get('author', instance.author)
+        instance.title = validated_data.get('author', instance.title)
+        instance.description = validated_data.get('author', instance.description)
+        instance.body = validated_data.get('author', instance.body)
+        instance.location = validated_data.get('author', instance.location)
+        instance.publication_date = validated_data.get('author', instance.publication_date)
+        instance.active = validated_data.get('author', instance.active)
+        instance.save()
+        return instance
