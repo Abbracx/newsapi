46e644ab9041954f20ef856b6931b06ded75d9d9
Abbracx <tankoraphael@gmail.com>
2022-02-19 16:52:23 +0100

    using ModelSerializers

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -1,35 +1,27 @@
 from rest_framework import serializers
+from datetime import datetime
+from django.utils.timesince import timesince
 from news.models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField(max_length=50)
-    title = serializers.CharField(max_length=10)
-    description = serializers.CharField(max_length=200)
-    body = serializers.CharField()
-    location = serializers.CharField(max_length=120)
-    publication_date = serializers.DateField()
-    active = serializers.BooleanField(default=True)
-    created_at = serializers.DateField()
-    updated_at = serializers.DateField()
+class ArticleSerializer(serializers.ModelSerializer):
+    # Definition of extra fields for my serializer
+    time_since_publication = serializers.SerializerMethodField()
 
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
+    class Meta:
+        model = Article
+        exclude = ("id",)
+        # fields = "__all__"
+        # fields = ("title", "description", "body")
 
-    def update(self, instance, validated_data):
-        instance.author = validated_data.get('author', instance.author)
-        instance.title = validated_data.get('author', instance.title)
-        instance.description = validated_data.get('author', instance.description)
-        instance.body = validated_data.get('author', instance.body)
-        instance.location = validated_data.get('author', instance.location)
-        instance.publication_date = validated_data.get('author', instance.publication_date)
-        instance.active = validated_data.get('author', instance.active)
-        instance.save()
-        return instance
+    def get_time_since_publication(self, object):
+        publication_date = object.publication_date
+        now = datetime.now()
+        time_delta = timesince(publication_date, now)
+        return time_delta
 
     # Object level validation 
     def validation(self, data):
-        if data["title"] === data["description"]:
+        if data["title"] == data["description"]:
             raise serializers.ValidationError("Title and description must be different")
         return data
 
